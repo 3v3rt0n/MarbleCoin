4111f8ffeb7e7f609cf9254e8fbcc50a77a1881c
3v3rt0n <everton.taques@gmail.com>
2018-09-17 18:21:36 -0300

    new version
    new version

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -13,7 +13,7 @@
  * for both marcod and marco-qt, to make it harder for attackers to
  * target servers or GUI users specifically.
  */
-const std::string CLIENT_NAME("Marco Core");
+const std::string CLIENT_NAME("Marco Polo");
 
 /**
  * Client version number
